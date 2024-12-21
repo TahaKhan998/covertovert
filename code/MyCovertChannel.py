7038830022e7000b6ec0b5702a4798293a216325
affanahmed2 <affanahmad323@gmail.com>
2024-12-21 20:50:38 +0300

    covert implemented

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,4 +1,7 @@
 from CovertChannelBase import CovertChannelBase
+from scapy.all import IP, UDP, DNS, sniff
+import warnings
+import time
 
 class MyCovertChannel(CovertChannelBase):
     """
@@ -9,17 +12,86 @@ class MyCovertChannel(CovertChannelBase):
         """
         - You can edit __init__.
         """
-        pass
-    def send(self, log_file_name, parameter1, parameter2):
+        
+    def send(self, log_file_name, receiver_ip, burst_time):
         """
         - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
-        binary_message = self.generate_random_binary_message_with_logging(log_file_name)
+
+        warnings.filterwarnings("ignore", category=SyntaxWarning)
+
+        binary_message =  self.generate_random_binary_message_with_logging(log_file_name)
+
+
+        for curr_bit in binary_message:
+            if (curr_bit == "1"):
+                packets = 2
+            else:
+                packets = 3
+
+            for i in range(packets):
+                packet = IP(dst = receiver_ip)/UDP()/DNS()
+                super().send(packet)
         
-    def receive(self, parameter1, parameter2, parameter3, log_file_name):
+            
+            time.sleep(burst_time)
+
+        time.sleep(burst_time)
+        packet = IP(dst = receiver_ip)/UDP()/DNS()
+        super().send(packet)
+        
+    def receive(self, burst_time, log_file_name):
         """
         - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
-        self.log_message("", log_file_name)
+        binary_message = ""
+        decoded_message = ""
+        packets_received = 0
+        last_time = time.time()
+        message_ended = False
+
+        def receive_packet(packet):
+
+            nonlocal binary_message, packets_received, last_time, decoded_message, message_ended
+
+        
+            curr_time = time.time()
+            time_difference = curr_time - last_time
+
+            packets_received += 1
+            last_time = curr_time
+
+          
+
+            if (time_difference > burst_time):
+                
+                if (packets_received == 2):
+                    binary_message += "1"
+                    
+
+                elif (packets_received == 3):
+                    binary_message += "0"
+                
+                
+                packets_received = 0
+
+                if (len(binary_message) == 8):
+                    decoded_message += self.convert_eight_bits_to_character(binary_message)
+                    binary_message = ""
+                    print("char =", decoded_message[-1])
+
+                    if (decoded_message[-1] == '.'):
+                        message_ended = True
+
+            
+
+        def end(_):
+            return message_ended
+
+        sniff(filter="udp port 53", prn=receive_packet, stop_filter = end)
+
+        self.log_message(decoded_message, log_file_name)
+
+
